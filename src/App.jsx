1b5ace2558c892d0654fca5757e056f0e753ab03
jainmayank12566 <jainmayank12566@gmail.com>
2024-01-16 23:25:28 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import {Sayhello} from './api/sayhello';
-import {useEffect, useRef, useState} from 'react';
+import { useRef, useState } from 'react';
 function App(){
     const[data,setdata]=useState("");
     const inputone=useRef();
-    async function handleclick(){
-        const data12=await Sayhello(inputone.current.value);
-        console.log(data12);
+    // async function handleclick(e){
+    //     e.preventDefault();
+    //     const data12=await Sayhello(inputone.current.value);
+    //     console.log(data12);
+    // }
+    //or
+    function handleclick(e){
+        e.preventDefault();
+        Sayhello(inputone.current.value)
+        .then((val)=>{
+            console.log(val);
+        })
     }
     return(
-        <div>
+        <form>
             <input type="text" value={data} onChange={(e)=>setdata(e.target.value)} ref={inputone}/>
-            <button onClick={handleclick}>change</button>
-        </div>
+            <button onClick={handleclick}>add</button>
+        </form>
     )
 }
 export default App;
